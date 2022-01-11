8d1788b63d46bf50106558ec496235237e56b6b2
Rodolfo Frainer <91473305+nellymaw@users.noreply.github.com>
2022-01-11 15:28:01 +0000

    add code to increment correct and incorrect answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,8 +48,10 @@ function checkAnswer(){
 
     if (isCorrect) {
         alert ('Hey! You got it right! :D~');
+        incrementScore();
     } else {
-        alert(`Aww... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`)
+        alert(`Aww... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -74,13 +76,20 @@ function calculateCorrectAnswer(){
         throw `Unimplemented operator ${operator}. Aborting!`
     }
 }
-
+/**
+ * gets current score from dom and increment it by 1
+ */
 function incrementScore(){
-
+    let oldScore = parseInt(document.getElementById('score').innerText);
+    document.getElementById('score').innerText = ++oldScore;
 }
 
+/**
+ * gets current score for incorrect answers from dom and increment it by 1
+ */
 function incrementWrongAnswer(){
-
+    let oldScore = parseInt(document.getElementById('incorrect').innerText);
+    document.getElementById('incorrect').innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1,operand2){
